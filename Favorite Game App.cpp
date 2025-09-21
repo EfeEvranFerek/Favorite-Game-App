3e64acd523133aee2aeb7d14e67b17f9f2ab8ad1
Efe Evran Ferek <efeevran.ferek@gmail.com>
2025-09-21 15:08:26 +0300

    Add files via upload

---
+++ Favorite Game App.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+string name, game;
+cout << "Enter your name please: ";
+getline (cin, name);
+cout << "Hello " << name << endl;
+cout << "That is a beatiful name " << name << endl ;
+cout << "What is your favorite game? " << endl;
+getline (cin, game) ;
+cout << game << " is fun! " << endl ;
+return 0;
+}
